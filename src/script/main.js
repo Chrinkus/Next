4c1d79e9fde97c95239b6a6891dd3351706e4c2a
Chrinkus <seeschickrun@gmail.com>
2016-02-09 13:06:09 -0600

    Preliminary work

---
+++ src/script/main.js
@@ -0,0 +1,14 @@
+(function() {
+	var canvas = document.getElementById("viewport");
+	var ctx = canvas.getContext("2d");
+
+	function roundedRect(ctx, x, y, w, h, r) {
+		// draw rect
+	}
+
+	function main(tStamp) {
+		window.requestAnimationFrame(main);
+
+	}
+	window.requestAnimationFrame(main);
+})();

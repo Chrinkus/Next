f5e770b7730182666274a4e3677ab1d4dc137ff5
Chrinkus <seeschickrun@gmail.com>
2016-02-17 14:11:51 -0600

    Created gamespace, linked start menu to it. Closes #1

--- src/script/main.js
+++ src/script/main.js
@@ -1,8 +1,10 @@
 (function() {
+    "use strict";
 	var canvas = document.getElementById("viewport");
 	var ctx = canvas.getContext("2d");
 	var CW = canvas.width;
 	var CH = canvas.height;
+    var stopStart, stopMain;
     
     // Input processing
     var keysDown = {};
@@ -17,11 +19,25 @@
     }, false);
 
     // Test content
-    var fields = ["Start", "Multiplayer", "Store", "Options", "Quit"];
+    var player = new Actor(CW / 2, CH / 2);
+    var fields = ["Start", "Options", "Quit"];
     var startMenu = new Menu(CW / 2, CH / 2, fields, 32);
+    startMenu.selections = function(field) {
+        switch (field) {
+            case "Start":
+                window.cancelAnimationFrame(stopStart);
+                stopMain = window.requestAnimationFrame(main);
+                break;
+            case "Options":
+                // do for options
+                break;
+            case "Quit":
+                // do for quit
+        }
+    }
 
-	function main(tStamp) {
-		window.requestAnimationFrame(main);
+	function start(tStamp) {
+		stopStart = window.requestAnimationFrame(start);
         var now = Math.floor(tStamp);
         var delta = now - then;
 
@@ -37,5 +53,23 @@
 
         then = now;
 	}
-	window.requestAnimationFrame(main);
+    stopStart = window.requestAnimationFrame(start);
+
+    function main(tStamp) {
+        stopMain = window.requestAnimationFrame(main);
+        var now = Math.floor(tStamp);
+        var delta = now - then;
+
+        ctx.clearRect(0, 0, CW, CH);
+
+        //Background
+        ctx.fillStyle = "#999";
+        ctx.fillRect(0, 0, CW, CH); 
+
+        //Actor
+        player.update(delta, keysDown);
+        player.draw(ctx);
+
+        then = now;
+    }
 })();

84db178107debdd5817d5952626d33646a00ed6f
Chrinkus <seeschickrun@gmail.com>
2016-02-20 01:05:17 -0600

    Added spritesheet processing, Closes #10
    On branch cs-sprite-animation
    Changes to be committed:
    modified:   index.html
    - linked animation.js
    modified:   src/script/actor.js
    - incorporated animation code into player constructor
    new file:   src/script/animation.js
    - created file
    - contains SpriteSheet constructor for loading a sheet
    - contains Animation constructor for extracting cycles from sheet

--- src/script/actor.js
+++ src/script/actor.js
@@ -8,22 +8,35 @@ function Actor(x, y) {
 function Player(x, y) {
     "use strict";
     Actor.call(this, x, y);
+    this.sheet = new SpriteSheet("/src/images/Red_Cube.png", 64, 64);
+    this.idle = new Animation(this.sheet, 15, 0, 1);
+    this.down = new Animation(this.sheet, 15, 2, 3);
+    this.up = new Animation(this.sheet, 15, 4, 5);
+    this.left = new Animation(this.sheet, 15, 6, 7);
+    this.right = new Animation(this.sheet, 15, 6, 7); // flip @ draw
+    this.anima = this.idle;
 }
 
 Player.prototype = Object.create(Actor.prototype);
 
 Player.prototype.update = function(delta, keysDown) {
-    if (KEY.W in keysDown) { this.y -= 1; }
-    if (KEY.S in keysDown) { this.y += 1; }
-    if (KEY.A in keysDown) { this.x -= 1; }
-    if (KEY.D in keysDown) { this.x += 1; }
+    this.anima = this.idle;
+    if (KEY.W in keysDown) { this.anima = this.up; this.y -= 1; }
+    if (KEY.S in keysDown) { this.anima = this.down; this.y += 1; }
+    if (KEY.A in keysDown) { this.anima = this.left; this.x -= 1; }
+    if (KEY.D in keysDown) { this.anima = this.right; this.x += 1; }
     if (KEY.ESC in keysDown) { this.pause = true; }
 }
 
 Player.prototype.draw = function(ctx) {
     ctx.save();
-    ctx.fillStyle = "#F00";
-    ctx.fillRect(this.x, this.y, 32, 32);
+    this.anima.update();
+    if (this.anima === this.right) {
+        ctx.scale(-1, 1);
+        this.anima.draw(ctx, -this.x - this.sheet.frameWidth, this.y);
+    } else {
+        this.anima.draw(ctx, this.x, this.y);
+    }
     ctx.restore();
 }
 

673f13b4512c5b5434eeadba6eaf5dbc5a48cc7d
Chrinkus <seeschickrun@gmail.com>
2016-02-21 22:20:21 -0600

    WIP: projectile work. Broken code. Sadness

--- src/script/actor.js
+++ src/script/actor.js
@@ -2,13 +2,51 @@ function Actor(x, y) {
     "use strict";
     this.x = x || 0;
     this.y = y || 0;
-    this.speed = 128;
-    this.pause = false;
+}
+
+function Projectile(x, y, facing) {
+    "use strict";
+    var that = this;
+    this.px = x + 32;               // half of player frame width
+    this.py = y + 32;               // half of player frame height
+    this.snapFace = facing;
+    this.speed = 512;
+
+    this.sheet = new SpriteSheet("/src/images/Red_Projectile.png", 64, 16);
+    this.fire = new Animation(this.sheet, 8, 0, 1);
+}
+
+Projectile.prototype = Object.create(Actor.prototype);
+
+Projectile.prototype.orient = function(ctx) {
+    ctx.save();
+    switch (this.snapFace) {
+        case "right":
+            // no rotate
+            break;
+        case "down":
+            ctx.rotate(Math.PI / 2);
+            break;
+        case "left":
+            ctx.rotate(Math.PI);
+            break;
+        case "up":
+            ctx.rotate(Math.PI * 1.5);
+            break;
+    }
+    this.fire.draw(ctx, this.px, this.py);
+    ctx.restore();
 }
 
 function Player(x, y) {
     "use strict";
     Actor.call(this, x, y);
+    this.speed = 128;
+    this.pause = false;
+    this.facing = "left";           // Default facing
+    this.projectiles = [];
+    this.delay = 0;
+
     this.sheet = new SpriteSheet("/src/images/Red_Cube.png", 64, 64);
     this.idle = new Animation(this.sheet, 15, 0, 1);
     this.down = new Animation(this.sheet, 15, 2, 3);
@@ -21,25 +59,41 @@ function Player(x, y) {
 Player.prototype = Object.create(Actor.prototype);
 
 Player.prototype.update = function(delta, keysDown) {
-    this.anima = this.idle;
+    // Character
+    this.delay += delta;
     this.speed = KEY.SHIFT in keysDown ? 256 : 128;
     if (KEY.W in keysDown) {
         this.anima = this.up;
+        this.facing = "up";
         this.y -= delta * this.speed;
     }
     if (KEY.S in keysDown) {
         this.anima = this.down;
+        this.facing = "down";
         this.y += delta * this.speed;
     }
     if (KEY.A in keysDown) {
         this.anima = this.left;
+        this.facing = "left";
         this.x -= delta * this.speed;
     }
     if (KEY.D in keysDown) {
         this.anima = this.right;
+        this.facing = "right";
         this.x += delta * this.speed;
     }
     if (KEY.ESC in keysDown) { this.pause = true; }
+
+    // Projectiles
+    if (this.delay > 100 && KEY.SPACE in keysDown && this.projectiles.length < 3) {
+        this.projectiles.unshift(new Projectile(this.x, this.y, this.facing));
+    }
+    if (this.projectiles.length) {
+        this.projectiles.forEach(function(proj, i, arr) {
+            proj.px += delta * proj.speed;
+            if (proj.px > 1000) { arr.slice(i); }
+        });
+    }
 }
 
 Player.prototype.draw = function(ctx) {
@@ -52,5 +106,10 @@ Player.prototype.draw = function(ctx) {
         this.anima.draw(ctx, this.x, this.y);
     }
     ctx.restore();
-}
 
+    if (this.projectiles.length) {
+        this.projectiles.forEach(function(proj) {
+            proj.orient(ctx);
+        });
+    }
+}
